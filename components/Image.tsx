9c1309395c05aa4b138422bf8414a947c1ae3916
旺仔小馒头 <wangzai@wangzaixiaomantous-MacBook-Pro.local>
2023-04-19 10:58:18 +0800

    添加自定义Imageloader

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,5 +1,5 @@
-import NextImage, { ImageProps } from 'next/image'
-
-const Image = ({ ...rest }: ImageProps) => <NextImage {...rest} />
+// eslint-disable-next-line jsx-a11y/alt-text
+// eslint-disable-next-line @next/next/no-img-element, jsx-a11y/img-redundant-alt
+const Image = ({ ...rest }) => <img alt="image" {...rest} />
 
 export default Image
