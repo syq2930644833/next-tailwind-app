8df59cc6863f0639c95b06cbaa441342753d1a32
旺仔小馒头 <wangzai@wangzaixiaomantous-MacBook-Pro.local>
2023-04-24 16:44:56 +0800

    添加域名备案;

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -22,8 +22,8 @@ export default function Footer() {
           <Link href="/">{siteMetadata.title}</Link>
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
-            Tailwind Nextjs Theme
+          <Link href="https://beian.miit.gov.cn/#/Integrated/recordQuery">
+            京ICP备2020036183号-1
           </Link>
         </div>
       </div>
